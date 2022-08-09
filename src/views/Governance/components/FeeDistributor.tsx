a868ac69cc0a4de033807003f2a024c06a74dded
Haz <haz@numisme.io>
2022-08-09 19:32:18 +0200

    Rename to Revenue Share

--- src/views/Governance/components/FeeDistributor.tsx
+++ src/views/Governance/components/FeeDistributor.tsx
@@ -7,7 +7,7 @@ import useTranslation from '../../../hooks/useTranslation'
 const FeeDistributor: React.FC = () => {
 	const translate = useTranslation()
 	return (
-		<Card title={translate('Fee Distribution')} icon="verticalbars">
+		<Card title={'Revenue Share'} icon="verticalbars">
 			<ClaimFees />
 		</Card>
 	)
